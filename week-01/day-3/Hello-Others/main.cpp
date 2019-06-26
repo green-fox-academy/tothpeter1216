a1cdb9cc59d5fffd69c506c1145f746fb8bd5e6e
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-26 15:09:14 +0200

    commit hello-others

---
+++ week-01/day-3/Hello-Others/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Bence!" << std::endl;
+    std::cout << "Hello, Dani!" << std::endl;
+    std::cout << "Hello, Gabor!" << std::endl;
+    return 0;
+}
