f32921918023ab23281ff61d3c7c8abc9ccbc898
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-26 16:11:52 +0200

    commit "coding-hours"

---
+++ week-01/day-3/coding-hours/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int dailyCodingHours = 6;
+    int semesterWeeks = 17;
+    int semesterDays = semesterWeeks * 5;
+    int semesterCodingHours = semesterDays * dailyCodingHours;
+    std::cout << "coding in a semester by an attendee: " << semesterCodingHours << std::endl;
+    int weeklyWorkingHours = 52;
+    int semesterWorkingHours = weeklyWorkingHours * semesterWeeks;
+    float percentageOfCodingHours = semesterCodingHours / semesterWorkingHours * 100;
+    std::cout << "percentage of the coding hours in the semester: " << percentageOfCodingHours << std::endl;
+    return 0;
+}
