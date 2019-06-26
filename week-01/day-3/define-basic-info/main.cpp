74972a8b478c9fe94b619dfa37f6cf17d051031d
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-26 17:07:51 +0200

    commit " define-basic-info"

---
+++ week-01/day-3/define-basic-info/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name = "Peti";
+    int age = 32;
+    double heightInM = 1.70;
+    bool marriedOrNot = false;
+
+
+    std::cout << "name: " <<name << std::endl;
+    std::cout << "age: " << age << std::endl;
+    std::cout << "height: " << heightInM << std::endl;
+    std::cout << "married or not: " << marriedOrNot << std::endl;
+    return 0;
+}
