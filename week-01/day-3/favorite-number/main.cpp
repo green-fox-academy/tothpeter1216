cfa5863da109df382db10bdee4a5d692fdce3ac1
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-26 16:33:07 +0200

    commit "favourite number"

---
+++ week-01/day-3/favorite-number/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args []) {
+    int myFavouriteNumber = 7;
+    std::string favouriteText = "My favorite number is: ";
+    std::cout << favouriteText << myFavouriteNumber << std::endl;
+    return 0;
+}
