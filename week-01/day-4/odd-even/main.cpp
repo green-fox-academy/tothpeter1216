3af931f65f9b74ff9ba0d2a7d5b417b15f34b385
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-27 15:30:00 +0200

    commit add "odd-even"

---
+++ week-01/day-4/odd-even/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    int randomNumber;
+    std::cout << "Write a number" << std::endl;
+    std::cin >> randomNumber;
+
+    int oddOrEvent = randomNumber % 2;
+            if (oddOrEvent == 0) {
+                std::cout << "The number is event." << std::endl;
+            } else{
+                std::cout << "The number is odd" << std::endl;
+            }
+            
+    return 0;
+}
