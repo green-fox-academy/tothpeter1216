4b997efb2040d3f3bf445b124416d609e362a7dc
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-27 16:58:33 +0200

    commit add "i-wont-cheat-on-the-exams"

---
+++ week-01/day-4/I-wont-cheat-on-the-exams/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int arg, char* args[]) {
+
+    std::string dontCheat = "I won't cheat on the exam!";
+    int sentenceCounter = 1;
+
+    while (sentenceCounter < 100){
+        std::cout << dontCheat << std::endl;
+        sentenceCounter++;
+    }
+    
+    return 0;
+}
