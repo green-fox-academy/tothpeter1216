90e7c0b908cad9ef75834fc757c724383f5c6afb
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-27 11:28:20 +0200

    commit "add cuboid"

---
+++ week-01/day-3/cuboid/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char* args[]){
+
+
+    double sidesOfCuboid = 10;
+    double surfaceOfCuboid = 6 * sidesOfCuboid * sidesOfCuboid;
+    double volumeOfCuboid = sidesOfCuboid * sidesOfCuboid * sidesOfCuboid;
+
+    std::cout << "Surface Area: " << surfaceOfCuboid << std::endl;
+    std::cout << "Volume: " << volumeOfCuboid << std::endl;
+
+    
+    return 0;
+}
