19c22fd5c39ce4430b62877409b530c2e0e26eb6
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-28 21:07:38 +0200

    commit add "swap-function"

---
+++ week-01/day-5/swap-function/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+void swap(int& a,int& b);
+
+int main(int argc, char* args[]) {
+
+    int a = 15;
+    int b = 23;
+
+    std::cout << "a= " << a << " b= " << b << std::endl;
+
+    swap(a,b);
+    std::cout << "a= " << a << " b= " << b << std::endl;
+
+    return 0;
+}
+
+void swap(int& a,int& b) {
+    std::swap(a, b);
+}
+
+// Create two integer variables: a=15, b=23
+// Create a function named swap that gets the 2 integers
+// as parameters and swaps its values.
+// Print the values before and after the function call
+// and make sure the values are printed in reversed order.
