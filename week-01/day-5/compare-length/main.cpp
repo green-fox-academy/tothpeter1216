246aa2d8a129f87301a96bfff628994ee812ad59
tothpeter1216 <tothpeter1216@gmail.com>
2019-06-28 13:43:33 +0200

    commit add "compare-length"

---
+++ week-01/day-5/compare-length/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    int p1[] = {1, 2, 3};
+    int p2[] = {4, 5};
+    int sizeOfP1 = sizeof(p1)/ sizeof(p1[0]);
+    int sizeOfP2 = sizeof(p2)/ sizeof(p2[0]);
+
+    if (sizeOfP1 < sizeOfP2){
+        std::cout << "p2 is longer" << std::endl;
+    }
+    
+    return 0;
+}
+
+
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print "p2 is longer" if `p2` has more elements than `p1`
--- week-01/day-5/third/main.cpp
+++ week-01/day-5/third/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char* args[]) {
-    
+
     int q[] = {4, 5, 6, 7};
 
     std::cout << "The third element is: " << q[2] << std::endl;
