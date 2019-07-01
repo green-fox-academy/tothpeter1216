bb9294432cbe4970679db3b62a2693acf964b6d0
tothpeter1216 <tothpeter1216@gmail.com>
2019-07-01 13:50:51 +0200

    commit add "simple-replace" and "url-fixer"

---
+++ week-02/day-1/url_fixer/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[])
+{
+    std::string url("https//www.reddit.com/r/nevertellmethebots");
+
+    url.replace (38, 1, "o" );
+    url.replace (40, 1, "d");
+    url.insert (5, ":");
+
+    // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+    // Also, the URL is missing a crucial component, find out what it is and insert it too!
+
+    std::cout << url << std::endl;
+
+    return 0;
+}
