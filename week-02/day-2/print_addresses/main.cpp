45c704616ffb23cd62336db9616a4c268b282c56
tothpeter1216 <tothpeter1216@gmail.com>
2019-07-02 14:18:19 +0200

    commit add "print addresses"

---
+++ week-02/day-2/print_addresses/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int fiveArray[5];
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Type in the " << i + 1 << ". number of the array." << std::endl;
+        std::cin >> fiveArray[i];
+
+    }
+    
+    for (int j = 0; j < 5; ++j) {
+
+        std::cout << &fiveArray[j] << std::endl;
+    }
+
+    // Create a program which accepts five integers from the console (given by the user)
+    // and store them in an array
+    // print out the memory addresses of the elements in the array
+
+    return 0;
+}
