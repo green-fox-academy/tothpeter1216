6ae4dbfc08abe571c395eeb1898e7a81f8ea5a43
tothpeter1216 <tothpeter1216@gmail.com>
2019-07-08 16:00:25 +0200

    commit add "copy-file"

---
+++ week-03/day-1/copy_file/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+
+
+
+bool fileCopy(std::string firstFile, std::string secondFile){
+    std::ifstream file(firstFile);
+    std::ofstream file2(secondFile);
+    std::string textCopy;
+    if (file.is_open() && file2.is_open()){
+        while (std::getline(file, textCopy))
+            file2 << textCopy << " ";
+        return true;
+    } else return false;
+}
+
+//bool fileCopy(std::string firstFile, std::string secondFile){
+//    std::ifstream file(firstFile);
+//    std::string textCopy;
+//    if (file.is_open()){
+//        while (std::getline(file, textCopy))
+//            std::cout << textCopy << std::endl;
+//
+//    }
+//    file.close();
+//
+//    std::ofstream file2(secondFile);
+//    if (file2.is_open()){
+//        file2 << textCopy;
+//        file2.close();
+//        return true;
+//    } else{
+//        return false;
+//    }
+//
+//
+//}
+
+
+int main() {
+
+    std::string firstFile;
+    std::string secondFile;
+
+    std::cout << "What is the location of your first file?" << std::endl;
+    std::cin >> firstFile;
+    std::cout << "Where do you want to copy?" << std::endl;
+    std::cin >> secondFile;
+
+    fileCopy(firstFile, secondFile);
+
+
+
+
+    return 0;
+}
