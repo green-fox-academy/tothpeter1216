12f5e61d3154f25fecbf57499a27221728aa89e3
tothpeter1216 <tothpeter1216@gmail.com>
2019-07-22 13:38:08 +0200

    commit add "element-finder"

---
+++ week-02/day-1/element-finder/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+std::string containsSeven(const std::vector<int>& numbers)
+{
+    bool containSeven = false;
+    for (int i = 0; i < numbers.size(); ++i) {
+        if (numbers[i] == 7){
+            containSeven = true;
+        }
+    }
+    if (containSeven == true){
+        return "Hoorray";
+    } else{
+        return "Nooooooo";
+    }
+
+}
+
+int main(int argc, char* args[])
+{
+    const std::vector<int> numbers = {1, 2, 3, 4, 5};
+    const std::vector<int> numbers2 = {6, 7, 8, 9, 10};
+
+    // Write a method that checks if the vector contains "7" if it contains return "Hoorray" otherwise return "Noooooo"
+
+    // Expected output: "Noooooo"
+    std::cout << containsSeven(numbers) << std::endl;
+
+    // Expected output: "Hoorray"
+    std::cout << containsSeven(numbers2) << std::endl;
+
+    return 0;
+}
