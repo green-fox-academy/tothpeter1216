45ead883be575606f9347aa9fe160072457d94ca
tothpeter1216 <tothpeter1216@gmail.com>
2019-08-05 13:16:41 +0200

    commit add "hello-others"

---
+++ week-06/day-1/hello_others/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+
+    printf("Hello, Levi!\n");
+    printf("Hello, Kornel!\n");
+    printf("Hello, Bence!\n");
+    
+    return 0;
+}
